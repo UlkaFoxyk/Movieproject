eb1546007c566c785b1e5e41de44ea7dd9287381
Ulka.Fox <ulyanapish@gmail.com>
2023-01-18 21:57:54 +0700

    added design and main application classes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import Movie from './components/Movie'
+import {useState, useEffect} from 'react'
+// import NotFound from './components/NotFound';
+// import {Spinner} from 'react-bootstrap';
+
+const movieApi = 'https://imdb-api.com/en/API/Search/k_7ondv4b8/'
+const movieTop = 'https://imdb-api.com/en/API/Top250Movies/k_7ondv4b8'
 
 function App() {
+  const [movie,setMovie] = useState([])
+  const [term, setTerm] = useState('')
+  const [loading, setLoading] = useState(true)
+  // const [error, setError] = useState(false)
+
+  const onHandleTerm = (e) => {
+    setTerm(e.target.value)
+  }
+  
+  useEffect(() => {
+    fetch(movieTop)
+    .then(res => res.json())
+    .then(res => {
+      setMovie(res.items)
+      setLoading(false)
+    })
+  }, [])
+
+ const onHandleSearch = (e) => {
+    e.preventDefault()
+    fetch(movieApi + term)
+    .then(res => res.json())
+    .then(res => setMovie(res.results))
+    setTerm('')
+
+
+  }
+
+
+//  const onNotFound = () => {
+//     setLoading(true)
+//     fetch(movieTop)
+//     .then(res => res.json())
+//     .then(res => {
+//       setMovie(res.items)
+//       setError(false)
+//       setLoading(false)
+//     })
+//  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <header>
+          <form action="submit" onSubmit={onHandleSearch}>
+           <input type="text" placeholder='Search...' value={term} onChange={onHandleTerm} />
+          </form>
+        </header>
+        <div className="movies">
+            {movie.map((elem) => <Movie key={elem.id} {...elem}/>)}
+        </div>
+    </>
   );
 }
 
